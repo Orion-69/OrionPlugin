fd355cd4819624c48843a1571d3cc4b7f3b4e692
Orion <95526396+Orion-69@users.noreply.github.com>
2021-12-11 17:58:35 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+//Dont change the name
+namespace OrionPlugin
+{
+    //Dont change the name
+    public class Plugin
+    {
+        //Dont change the name
+        //DO NOT add parameters (RunPlugin(object obj1)
+        public static void RunPlugin()
+        {
+            //Add all the executing code here, for example:
+            //Method1();
+            //Class2.Method2();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("OrionPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrionPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a6d86f0e-832c-4cd2-ae4b-45a1e68cd1ff")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
